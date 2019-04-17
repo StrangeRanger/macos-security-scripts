65a4bcdee890543d3074f115c8631bcaf0312d83
StrangeRanger <strangeranger01@gmail.com>
2019-04-17 15:07:30 -0700

    build: add code to disable camera

--- hardening/camera.sh
+++ hardening/camera.sh
@@ -5,6 +5,7 @@ NC='\033[0m'
 
 read -p "csrutil must be disabled to continue" 
 
+# asks user if they disabled csrutil
 while true; do
 	read -p "Have you disabled csrutil? [y,n] " doit
 	doit=$( echo "$doit" | tr '[:upper:]' '[:lower:]')
@@ -12,7 +13,7 @@ while true; do
 		break
 	elif [ $doit == "n" ] || [ $doit == "no" ]; then
 		read -p "We will now disable csrutil, then the script will stop. You must reboot your computer then rerun the script."
-		#csrutil disable
+		csrutil disable
 		echo "Disabled csrutil. Now please reboot your computer and then re-execute this script"
 		exit 1
 	else
@@ -22,4 +23,17 @@ done
 
 read -p "We will now disable your all laptop camera. \nWARNING!!! By doing this, you will completely disable the software components behind the built-in camera on your computer. Press 'enter' to continue and 'Ctrl + C' to cancel."
 
-#input code to disable camera
+# disables camera software components
+sudo chmod a-r /System/Library/Frameworks/CoreMediaIO.framework/Versions/A/Resources/VDC.plugin/Contents/MacOS/VDC
+sudo chmod a-r /System/Library/PrivateFrameworks/CoreMediaIOServicesPrivate.framework/Versions/A/Resources/AVC.plugin/Contents/MacOS/AVC
+sudo chmod a-r /System/Library/QuickTime/QuickTimeUSBVDCDigitizer.component/Contents/MacOS/QuickTimeUSBVDCDigitizer
+sudo chmod a-r /Library/CoreMediaIO/Plug-Ins/DAL/AppleCamera.plugin/Contents/MacOS/AppleCamera
+sudo chmod a-r /Library/CoreMediaIO/Plug-Ins/FCP-DAL/AppleCamera.plugin/Contents/MacOS/AppleCamera
+
+echo -e "Successfully disabled all software components that control camera\n"
+read -p "We will now re-enable csrutil"
+
+csrutil enable
+
+echo "Successfully re-enabled csrutil"
+echo "Done"
