5e02813e2a6d5fc2e126ce61159db1efcb8f861b
StrangeRanger <strangeranger01@gmail.com>
2020-07-28 16:13:27 -0800

    style: coding style now follows personal bash script style

--- hardening/disable-root.sh
+++ hardening/disable-root.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-read -p "We will now disable root. When prompted, you will need to input your password."
-dsenableroot -d 
-echo "Done!"
+################################################################################
+#
+# Disable Root
+# ------------
+# Disables the root account
+#
+################################################################################
+#
+# [ Pre-main ]
+#
+################################################################################
+#
+    # Makes gloabl variables accessible
+    . ../globalvar.sh
+
+    # Checks to see if this script was executed with root privilege
+    if ((EUID == 0)); then 
+        echo "${red}Do not run this script as root or with root privilege${nc}" >&2
+        echo -e "\nExiting..."
+        #exit 1
+    fi
+
+#
+################################################################################
+#
+# [  Main ]
+#
+################################################################################
+#
+    read -p "We will now disable the root account. Press [Enter] to continue."
+    dsenableroot -d
