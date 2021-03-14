7f45cb7abe5fd28d270ee7c1b9634f268b133a93
StrangeRanger <strangeranger01@randomserver.xyz>
2021-03-14 14:49:18 -0700

    style: change comment and programming style

--- hardening/root-disabler.sh
+++ hardening/root-disabler.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-
-################################################################################
 #
-# Root Disabler
-# -------------
 # Disables the root account
 #
 # Version: v1.0.1
-# License: MIT License 
+# License: MIT License
 #   Copyright (c) 2020-present Hunter T.
 #
 ################################################################################
-#
-    red=$'\033[1;31m'
-    nc=$'\033[0m'
 
-    # Checks to see if this script was executed with root privilege
-    if ((EUID == 0)); then 
-        echo "${red}Do not run this script as root or with root privilege${nc}" >&2
-        echo -e "\nExiting..."
-        exit 1
-    fi
+red=$'\033[1;31m'
+nc=$'\033[0m'
 
-    read -p "We will now disable the root account. Press [Enter] to continue."
-    dsenableroot -d
+# Checks to see if this script was executed with root privilege
+if ((EUID == 0)); then
+    echo "${red}Do not run this script as root or with root privilege${nc}" >&2
+    echo -e "\nExiting..."
+    exit 1
+fi
+
+read -rp "We will now disable the root account. Press [Enter] to continue."
+dsenableroot -d
