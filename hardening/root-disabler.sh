1f41e48632645597640d6e39ffb470e828901f61
Hunter T <strangeranger01@randomserver.xyz>
2022-07-11 11:41:38 +0200

    style: NOT is upper case for emphasis and more

--- hardening/root-disabler.sh
+++ hardening/root-disabler.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Disable the root account.
 #
 # Version: v1.0.1
 # License: MIT License
-#          Copyright (c) 2020-2021 Hunter T.
+#          Copyright (c) 2020-2022 Hunter T. (StrangeRanger) 
 #
 ########################################################################################
 
@@ -13,7 +13,7 @@ nc=$'\033[0m'
 
 ## Check if the script was executed with root privilege.
 if [[ $EUID = 0 ]]; then
-    echo "${red}Do not run this script as root or with root privilege${nc}" >&2
+    echo "${red}Do NOT run this script as root or with root privilege${nc}" >&2
     echo -e "\nExiting..."
     exit 1
 fi
