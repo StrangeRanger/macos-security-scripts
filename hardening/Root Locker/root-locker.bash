7ae4d60e272d8fcef7f23a19389be751d8d5684a
Hunter T. <strangeranger@hthompson.dev>
2024-09-11 11:11:44 -0700

    No longer requires root perms

--- hardening/Root Locker/root-locker.bash
+++ hardening/Root Locker/root-locker.bash
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 #
-# This script locks the root account. This prevents users from logging into the root
-# account via `su` or the GUI. Note that it does NOT prevent users from becoming root
-# via methods such as `sudo su`.
+# This script disables the root account and removes its password.
 #
-# Version: v1.0.3
+# Version: v1.0.4
 # License: MIT License
 #          Copyright (c) 2020-2024 Hunter T. (StrangeRanger)
 #
 ########################################################################################
-
-C_RED="$(printf '\033[1;31m')"
-C_NC="$(printf '\033[0m')"
-
-
-## Check if the script was executed with root privilege.
-if [[ $EUID != 0 ]]; then
-    echo "${C_RED}This script requires root privilege${C_NC}" >&2
-    echo -e "\nExiting..."
-    exit 1
-fi
-
 
 read -rp "We will now disable the root account. Press [Enter] to continue."
 dsenableroot -d
