268c821a2e5d165f7474734998faaf048b618320
Hunter T. <strangeranger@hthompson.dev>
2024-12-02 13:37:28 -0800

    Update comments

--- hardening/Root Locker/root-locker.bash
+++ hardening/Root Locker/root-locker.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #
-# This script disables the root account and removes its password.
+# This script disables the root account, preventing users from directly logging in as
+# root.
+#
+# Note:
+#   Disabling the root account doesn't prevent users from using something like `sudo su`
+#   to gain root access.
 #
 # Version: v1.0.4
 # License: MIT License
